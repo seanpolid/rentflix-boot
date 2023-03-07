d8dda91efc9624eb2ae4725f20cc452964bb1eee
Sean Polidori <seanpolidori0@gmail.com>
2023-03-07 11:29:18 -0500

    Initial commit

---
+++ rentflix/src/main/java/com/rentflix/RentflixApplication.java
@@ -0,0 +1,13 @@
+package com.rentflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentflixApplication.class, args);
+	}
+
+}
---
+++ rentflix/src/test/java/com/rentflix/RentflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
